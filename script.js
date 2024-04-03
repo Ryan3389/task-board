edfcb137075d3b94517a65671c04e0b6118753b7
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-04-02 23:23:32 -0400

    added code to addTask function and added styles

--- script.js
+++ script.js
@@ -19,7 +19,43 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event) {
+    event.preventDefault();
 
+    //avoid any empty inputs
+    if ($('#task-title').val() === "" || $('#task-date').val() === "" || $('#task-desc').val() === "") {
+        alert("Please fill in all input fields");
+        return;
+    }
+    //dynamically adding classes based on due date
+    let statusClass;
+    const taskDesc = $('#task-desc').val().toLowerCase();
+    if (taskDesc === 'past due') {
+        statusClass = 'past-due';
+    } else if (taskDesc === 'in progress') {
+        statusClass = 'in-progress';
+    } else if (taskDesc === 'done') {
+        statusClass = 'done';
+    } else {
+        statusClass = ''; // Default status class
+    }
+    //pushing new task into task arr
+    const newTask = {
+        id: generateTaskId(),
+        taskTitle: $('#task-title').val(),
+        taskDate: $('#task-date').val(),
+        taskDesc: taskDesc,
+        status: statusClass
+    };
+
+    taskData.push(newTask);
+    //setting local storage for task data
+    localStorage.setItem('taskData', JSON.stringify(taskData));
+
+    createTaskCard(newTask); // Render only the newly added task card
+    //clears input values after submission
+    $('#task-title').val('');
+    $('#task-date').val('');
+    $('#task-desc').val('');
 }
 
 // Todo: create a function to handle deleting a task
